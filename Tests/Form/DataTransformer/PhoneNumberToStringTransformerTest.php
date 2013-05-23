22901e594c39282a345df6eb9a64e0f279c96de4
thewilkybarkid <chriswilkinson84@gmail.com>
2013-05-23 13:12:51 +0100

    Add data transformer test

---
+++ Tests/Form/DataTransformer/PhoneNumberToStringTransformerTest.php
@@ -0,0 +1,98 @@
+<?php
+
+/*
+ * This file is part of the Symfony2 PhoneNumberBundle.
+ *
+ * (c) University of Cambridge
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Misd\PhoneNumberBundle\Tests\Form\DataTransformer;
+
+use libphonenumber\PhoneNumber;
+use libphonenumber\PhoneNumberFormat;
+use libphonenumber\PhoneNumberUtil;
+use Misd\PhoneNumberBundle\Form\DataTransformer\PhoneNumberToStringTransformer;
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * Phone number to string transformer test.
+ *
+ * @author Chris Wilkinson <chris.wilkinson@admin.cam.ac.uk>
+ */
+class PhoneNumberToStringTransformerTest extends TestCase
+{
+    public function testConstructor()
+    {
+        $transformer = new PhoneNumberToStringTransformer();
+
+        $this->assertInstanceOf('Symfony\Component\Form\DataTransformerInterface', $transformer);
+    }
+
+    /**
+     * @dataProvider transformProvider
+     */
+    public function testTransform($defaultRegion, $format, $actual, $expected)
+    {
+        $transformer = new PhoneNumberToStringTransformer($defaultRegion, $format);
+
+        $phoneNumberUtil = PhoneNumberUtil::getInstance();
+        if (null !== $actual) {
+            $phoneNumber = $phoneNumberUtil->parse($actual, $defaultRegion);
+        } else {
+            $phoneNumber = null;
+        }
+
+        $transformed = $transformer->transform($phoneNumber);
+
+        $this->assertSame($expected, $transformed);
+    }
+
+    /**
+     * 0 => Default region
+     * 1 => Format
+     * 2 => Actual value
+     * 3 => Expected result
+     */
+    public function transformProvider()
+    {
+        return array(
+            array('ZZ', PhoneNumberFormat::INTERNATIONAL, null, ''),
+            array('ZZ', PhoneNumberFormat::INTERNATIONAL, '+441234567890', '+44 1234 567890'),
+            array('GB', PhoneNumberFormat::NATIONAL, '01234567890', '01234 567890'),
+        );
+    }
+
+    /**
+     * @dataProvider reverseTransformProvider
+     */
+    public function testReverseTransform($defaultRegion, $actual, $expected)
+    {
+        $transformer = new PhoneNumberToStringTransformer($defaultRegion);
+
+        $transformed = $transformer->reverseTransform($actual);
+
+        if ($transformed instanceof PhoneNumber) {
+            $transformed = (string) $transformed;
+        }
+
+        $this->assertSame($expected, $transformed);
+    }
+
+    /**
+     * 0 => Default region
+     * 1 => Actual value
+     * 2 => Expected result
+     */
+    public function reverseTransformProvider()
+    {
+        return array(
+            array('ZZ', null, null),
+            array('ZZ', 'foo', 'foo'),
+            array('ZZ', '+44 1234 567890', '+441234567890'),
+            array('GB', '01234 567890', '+441234567890'),
+        );
+    }
+}
