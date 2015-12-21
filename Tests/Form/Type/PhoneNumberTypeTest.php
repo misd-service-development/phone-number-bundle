a941972a753844e95dd7e866d55b91a4f0f7f55c
JHGitty <jhgitty@trash-mail.com>
2015-12-21 16:15:30 +0100

    Symfony 3 Compatibility

--- Tests/Form/Type/PhoneNumberTypeTest.php
+++ Tests/Form/Type/PhoneNumberTypeTest.php
@@ -27,7 +27,12 @@ class PhoneNumberTypeTest extends TypeTestCase
      */
     public function testDefaultFormatting($input, $options, $output)
     {
-        $type = new PhoneNumberType();
+        if (method_exists('Symfony\\Component\\Form\\FormTypeInterface', 'getName')) {
+            $type = new PhoneNumberType();
+        } else {
+            $type = 'Misd\\PhoneNumberBundle\\Form\\Type\\PhoneNumberType';
+        }
+        
         $form = $this->factory->create($type, null, $options);
 
         $form->submit($input);
