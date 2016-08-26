34d1f4c7194103ef6df690655ca67fec66463847
Toni Rudolf <toni.rudolf@gmail.com>
2016-08-26 08:23:46 +0200

    readd classes as deprecated

---
+++ Templating/Helper/PhoneNumberFormatHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the Symfony2 PhoneNumberBundle.
+ *
+ * (c) University of Cambridge
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Misd\PhoneNumberBundle\Templating\Helper;
+
+/**
+ * @deprecated PhoneNumberFormatHelper is deprecated and will be removed in 2.0. Use PhoneNumberHelper instead
+ */
+class PhoneNumberFormatHelper extends PhoneNumberHelper {}
---
+++ Twig/Extension/PhoneNumberFormatExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Symfony2 PhoneNumberBundle.
+ *
+ * (c) University of Cambridge
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Misd\PhoneNumberBundle\Twig\Extension;
+
+use Misd\PhoneNumberBundle\Templating\Helper\PhoneNumberHelper;
+use Twig_Extension as Extension;
+use Twig_SimpleFunction as SimpleFunction;
+
+/**
+ * @deprecated PhoneNumberFormatExtension is deprecated and will be removed in 2.0. Use PhoneNumberHelperExtension instead
+ */
+class PhoneNumberFormatExtension extends PhoneNumberHelperExtension {}
